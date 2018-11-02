6ae842a466040daa9673fb9fc5195185b989869c
micah-jaffe <micah.jaffe95@gmail.com>
2018-11-02 15:21:02 -0700

    Implement HashMap class

--- MyHashMap/lib/p05_hash_map.rb
+++ MyHashMap/lib/p05_hash_map.rb
@@ -3,6 +3,7 @@ require_relative 'p04_linked_list'
 class HashMap
   include Enumerable
   attr_accessor :count
+  attr_reader :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -28,18 +29,25 @@ class HashMap
   end
 
   def delete(key)
+    bucket(key).remove(key)
+    @count -= 1
   end
 
-  def each
+  def each(&prc)
+    @store.each do |bucket|
+      bucket.each do |node|
+        prc.call(node.key, node.val)
+      end 
+    end 
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -51,16 +59,16 @@ class HashMap
   end
 
   def resize!
-    new_set = self.class.new(num_buckets * 2)
+    new_hashmap = self.class.new(num_buckets * 2)
     
     @store.each do |bucket| 
-      bucket.each do |el|
-        new_set.insert(el)
+      bucket.each do |node|
+        new_hashmap.set(node.key, node.val)
       end 
     end 
     
-    @store = new_set.store
-    @count = new_set.count
+    @store = new_hashmap.store
+    @count = new_hashmap.count
   end
 
   def bucket(key)
